06de220c20fbf4c168e408a73e68cef3ac843d41
amin <amin57007@gmail.com>
2020-11-17 10:49:56 -0800

    add soldiers, name and password parameter and its methods to card

---
+++ Player.java
@@ -0,0 +1,46 @@
+public class Player {
+    private String username;
+    private String password;
+    private String name;
+    private int score;
+    private int coin;
+    private int soldiers;
+    public static void main(String[] args) {}
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setCoin(int coin) {
+        this.coin = coin;
+    }
+    public int getCoin() {
+        return coin;
+    }
+    public void setScore(int score) {
+        this.score = score;
+    }
+    public int getScore() {
+        return score;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public int getPassword() {
+        return password;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getName() {
+        return name;
+    }
+    public void setSoldiers(int soldiers) {
+        this.soldiers = soldiers;
+    }
+    public int getSoldiers() {
+        return soldiers;
+    }
+
+}
