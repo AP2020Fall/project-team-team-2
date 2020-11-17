f63c6ce8685d1f4f5527c8b9f60123003240cb28
amin <amin57007@gmail.com>
2020-11-17 11:41:01 -0800

    add position to country class

--- Country.java
+++ Country.java
@@ -2,6 +2,8 @@ public class Country {
     private String name;
     private Player Owner;
     private int soldiers;
+    private int xPos;
+    private int yPos;
     public static void main(String[] args) {}
     public void setName(String name) {
         this.name = name;
@@ -21,5 +23,8 @@ public class Country {
     public int getSoldiers() {
         return soldiers;
     }
-
+    public void setXPos(int xPos) { this.xPos() = xPos; }
+    public void getXPos() { return xPos; }
+    public void setYPos(int yPos) { this.yPos() = yPos; }
+    public void getYPos() { return yPos; }
 }
