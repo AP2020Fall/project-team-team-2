7de4c7c8483b6b1c73bf6996a5ade102d675dce8
amin <amin57007@gmail.com>
2020-11-30 10:08:41 -0800

    add incPlayerScore method to MatchCardController file

--- src/main/java/controller/MatchCardController.java
+++ src/main/java/controller/MatchCardController.java
@@ -5,7 +5,12 @@ import model.Player;
 public class MatchCardController {
 
     private Player player;
+    private int tempScore;
 
-
+    public void incPlayerScore(Player player, int score){
+        tempScore = player.getScore();
+        tempScore = tempScore + score;
+        player.setScore(tempScore);
+    }
 
 }
