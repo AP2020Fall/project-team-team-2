7cf63873ad087964a9b20f23072dcc4c89945c97
alxelr2014 <I@$mint16p17>
2020-12-06 17:54:24 +0330

    finish registerMenu + Controller

--- src/main/java/controller/RegisterController.java
+++ src/main/java/controller/RegisterController.java
@@ -5,6 +5,7 @@ import model.Account;
 import model.Admin;
 import model.Player;
 import view.AdminMainMenu;
+import view.PlayerMainMenu;
 
 import java.util.ArrayList;
 
@@ -13,22 +14,24 @@ public class RegisterController extends Controller {
 
     public void createAccount(String username, String password, ArrayList<String> additionalInfo) {
         if (!Admin.isAdminExist()) {
-            createAdmin(username, password, additionalInfo);
+           new AdminMainMenu(createAdmin(username, password, additionalInfo));
             //go to admin account menu
         } else {
-            createPlayer(username, password, additionalInfo);
+            new PlayerMainMenu(createPlayer(username, password, additionalInfo));
             //go to player account menu
         }
         //LoginController.login(username); maybe make login static
     }
 
-    private void createPlayer(String username, String password, ArrayList<String> additionalInfo) {
+    private Player createPlayer(String username, String password, ArrayList<String> additionalInfo) {
         Player player = new Player(additionalInfo.get(0), additionalInfo.get(1), username, generateId(), password, additionalInfo.get(2), additionalInfo.get(3), 0, Double.parseDouble(additionalInfo.get(4)), 0);
         Account.getAllAccounts().add(player);
+        return player;
     }
 
-    private void createAdmin(String username, String password, ArrayList<String> additionalInfo) {
+    private Admin createAdmin(String username, String password, ArrayList<String> additionalInfo) {
         Admin admin = new Admin(additionalInfo.get(0), additionalInfo.get(1), username, generateId(), password, additionalInfo.get(2), additionalInfo.get(3));
         Account.getAllAccounts().add(admin);
+        return admin;
     }
 }
