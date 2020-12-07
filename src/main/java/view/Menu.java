32a37ae0d73d6327a209fca14283334cf58d536d
mohamadamin karami <mohamadaminkarami80@gmail.com>
2020-12-07 21:47:05 +0330

    add method createLocalDate

--- src/main/java/view/Menu.java
+++ src/main/java/view/Menu.java
@@ -3,6 +3,7 @@ package view;
 
 import model.Account;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -19,15 +20,15 @@ public abstract class Menu {
         this.account = account;
     }
 
-    protected boolean checkEmail(String email){
-        if (email.matches("^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$")){
+    protected boolean checkEmail(String email) {
+        if (email.matches("^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$")) {
             return true;
         }
         return false;
     }
 
-    protected boolean checkPhoneNumber(String phoneNumber){
-        if (phoneNumber.matches("09\\d{9}")){
+    protected boolean checkPhoneNumber(String phoneNumber) {
+        if (phoneNumber.matches("09\\d{9}")) {
             return true;
         }
         return false;
@@ -40,5 +41,14 @@ public abstract class Menu {
 
     protected void viewAccountMenu(Account account) {
         new AccountMenu(account);
+    }
+
+    protected LocalDate createLocalDate(String input) {
+        String[] splitInput = input.split("\\/");
+        int year = Integer.parseInt(splitInput[0]);
+        int month = Integer.parseInt(splitInput[1]);
+        int day = Integer.parseInt(splitInput[2]);
+        LocalDate localDate = LocalDate.of(year, month, day);
+        return localDate;
     }
 }
