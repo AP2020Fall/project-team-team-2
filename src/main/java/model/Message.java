f2d014fda424b2b9c05aeb68f0f8ecd61ac497cb
alxelr2014 <I@$mint16p17>
2020-12-08 15:36:15 +0330

    Message Class + Player Messages

---
+++ src/main/java/model/Message.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Message {
+    private final String message;
+    private final String messageId;
+    private final Player player;
+    private final LocalDateTime time;
+    private boolean read;
+    public Message(String message,String messageId,Player player,LocalDateTime time)
+    {
+        this.message = message;
+        this.messageId = messageId;
+        this.player = player;
+        this.time = time;
+        this.read = false;
+    }
+    public void sendMessage()
+    {
+
+    }
+}
