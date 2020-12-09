08d642f7d348471f6a142572775846c4eb62adcd
amin <amin57007@gmail.com>
2020-12-09 09:57:41 -0800

    write matchCardAddSoldiers method in MatchCardController file

--- src/main/java/controller/MatchCardController.java
+++ src/main/java/controller/MatchCardController.java
@@ -7,8 +7,12 @@ public class MatchCardController {
     private Player player;
     private int addedSoldiers;
 
+    public MatchCardController(Player player){
+        this.player = player();
+        addedSoldiers = 0;
+    }
+
     public void incPlayerSoldier(Player player, int soldierNumber) {
         player.addNewSoldiers(soldierNumber);
     }
-
 }
