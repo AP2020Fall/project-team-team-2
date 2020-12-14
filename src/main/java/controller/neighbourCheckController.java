3cf19ba6d761044f1d5dcf5cf027a650ba519467
amin <amin57007@gmail.com>
2020-12-14 11:40:35 -0800

    add new control file to check neighbourhood

---
+++ src/main/java/controller/neighbourCheckController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import model.Player;
+import model.Country;
+import model.Map;
+
+public class neighbourCheckController {
+    Player currentPlayer;
+    Country sourceContry;
+    Country DestinationCountry;
+
+}
