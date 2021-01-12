282bd2648c0eb19a34b074a0045b9b350de49f85
alxelr2014 <I@$mint16p17>
2021-01-12 14:18:34 +0330

    adding avatar to tableviews

--- src/main/java/model/Entry/FriendEntry.java
+++ src/main/java/model/Entry/FriendEntry.java
@@ -1,6 +1,7 @@
 package model.Entry;
 
 import javafx.scene.control.Hyperlink;
+import javafx.scene.image.ImageView;
 import model.Player;
 import view.TabHandler;
 import view.player.PlayerProfileView;
@@ -10,8 +11,12 @@ public class FriendEntry {
     private String name;
     private Hyperlink remove = new Hyperlink("Remove");
     private Hyperlink view = new Hyperlink("View");
+    private ImageView imageView;
     public FriendEntry(Player friend,Player player) {
         name = friend.getUsername();
+        imageView = new ImageView(friend.getImage());
+        imageView.setFitHeight(48);
+        imageView.setFitWidth(48);
         remove.setOnAction(event -> {
             //System.out.println("Removing must be implemented");
             player.removeFriend(friend);
@@ -34,5 +39,9 @@ public class FriendEntry {
 
     public Hyperlink getView() {
         return view;
+    }
+
+    public ImageView getImageView() {
+        return imageView;
     }
 }
