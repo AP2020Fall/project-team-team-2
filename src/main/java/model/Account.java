68a32961ff1d5cb1a7842bada91db5638bc094c7
alxelr2014 <I@$mint16p17>
2021-02-03 00:45:15 +0330

    sql for Account.java

--- src/main/java/model/Account.java
+++ src/main/java/model/Account.java
@@ -26,8 +26,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-public abstract class Account extends Application {
-    protected static ArrayList<Account> allAccounts = new ArrayList<>();
+public abstract class Account {
     private static String rememberMeUsername;
     private static String rememberMePassword;
     private String bio;
@@ -39,16 +38,9 @@ public abstract class Account extends Application {
     private String email;
     private String phoneNumber;
     private LocalDate registerDay;
+    private boolean isAdmin = false;
     private boolean isRobot = false;
     private String avatar;
-
-    public static Admin getAdmin() {
-        for(Account account: allAccounts)
-            if(account instanceof Admin)
-                return (Admin)account;
-            return null;
-    }
-
 
     public static String getRememberMePassword() {
         return rememberMePassword;
@@ -65,87 +57,10 @@ public abstract class Account extends Application {
     public static void setRememberMeUsername(String rememberMeUsername) {
         Account.rememberMeUsername = rememberMeUsername;
     }
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        System.out.println("shit happened here");
-    }
-
-    public static void main(String[] args) {
-        Account test = new Player("Emad","Zing","Emad","12345","bye","Emad.z","434311",
-                4.0);
-        SQLConnector.insertInDatabase(test.SQLAccountToMap(),"players");
-        System.out.println(test.getImageURL());
-    }
-
-    private Map<String,Object> SQLAccountToMap()
-    {
-        Map<String,Object> resultMap = new HashMap<>();
-        resultMap.put("first_name",this.firstName);
-        resultMap.put("last_name",this.lastName);
-        resultMap.put("username",this.username);
-        resultMap.put("hash_password",this.password);
-        resultMap.put("email_address",this.email);
-        resultMap.put("phone_number",this.phoneNumber);
-        resultMap.put("register_date",this.registerDay);
-        resultMap.put("avatar_address",this.avatar);
-        resultMap.put("player_id",this.accountId);
-        resultMap.put("bio",this.bio);
-        resultMap.put("is_admin",this.isRobot);
-        return resultMap;
-    }
-
-
-    private Map<String, List<Object>> SQLGetCurrentPlayer()
-    {
-        java.util.Map<String, Object> newMap = new HashMap<>();
-        newMap.put("player_id", this.accountId);
-        Map<String, List<Object>> thisAccount =
-                SQLConnector.selectFromDatabase(newMap, "players");
-        System.out.println(thisAccount);
-        if(thisAccount == null || thisAccount.isEmpty())
-        {
-            System.err.println("Not a valid entry");
-            return null;
-        }
-        return thisAccount;
-    }
-
-    public Image getImage() {
-        return new Image(getImageURL());
-        //File file = new File("database\\accounts\\images\\" + accountId + ".jpg");
-        //return new Image(file.toURI().toString());
-    }
-    public  String getImageURL()
-    {
-        Map<String,List<Object>> accountData = this.SQLGetCurrentPlayer();
-        if(accountData != null) {
-            System.out.println(accountData);
-            return (String) accountData.get("avatar_address").get(0);
-        }
-        return "";
-        //return avatar;
-    }
-
-
-    public void setImage(String url) {
-        this.avatar = saveImageToFile(new Image(url),this.accountId);
-    }
 
     public Account(String botName, String username) {
         this.firstName = botName;
         this.username = username;
-    }
-
-    public int getDayOfRegister() {
-        return (int) ChronoUnit.DAYS.between(registerDay, LocalDate.now());
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
     }
 
     public Account(String firstName, String lastName, String accountName, String accountId, String password, String email, String phoneNumber) {
@@ -177,6 +92,10 @@ public abstract class Account extends Application {
         return accountId;
     }
 
+    public String getBio() {
+        return bio;
+    }
+
     public String getPassword() {
         return password;
     }
@@ -189,7 +108,32 @@ public abstract class Account extends Application {
         return phoneNumber;
     }
 
+    public int getDayOfRegister() {
+        return (int) ChronoUnit.DAYS.between(registerDay, LocalDate.now());
+    }
+
+    public Image getImage() {
+        return new Image(getImageURL());
+        //File file = new File("database\\accounts\\images\\" + accountId + ".jpg");
+        //return new Image(file.toURI().toString());
+    }
+
+    public String getImageURL() {
+       /* Map<String, List<Object>> accountData = this.SQLGetCurrentPlayer();
+        if (accountData != null) {
+            System.out.println(accountData);
+            return (String) accountData.get("avatar_address").get(0);
+        }
+        return "";*/
+        return avatar;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
     public void setPassword(String password) {
+
         this.password = password;
     }
 
@@ -213,30 +157,96 @@ public abstract class Account extends Application {
         this.username = username;
     }
 
+    public void setImage(String url) {
+        this.avatar = saveImageToFile(new Image(url), this.accountId);
+    }
 
-    public static ArrayList<Account> getAllAccounts() {
-        return allAccounts;
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    private Map<String, Object> SQLAccountToMap() {
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("first_name", this.firstName);
+        resultMap.put("last_name", this.lastName);
+        resultMap.put("username", this.username);
+        resultMap.put("hash_password", this.password);
+        resultMap.put("email_address", this.email);
+        resultMap.put("phone_number", this.phoneNumber);
+        resultMap.put("register_date", this.registerDay.toString());
+        resultMap.put("avatar_address", this.avatar);
+        resultMap.put("player_id", this.accountId);
+        resultMap.put("bio", this.bio);
+        resultMap.put("is_admin", isAdmin ? 1 : 0);
+        return resultMap;
+    }
+    private static Admin createAdmin(Map<String,List<Object>> admin)
+    {
+        return new Admin((String) admin.get("first_name").get(0), (String) admin.get("last_name").get(0),
+                (String) admin.get("username").get(0), (String) admin.get("player_id").get(0),
+                (String) admin.get("hash_password").get(0), (String) admin.get("email_address").get(0),
+                (String) admin.get("phone_number").get(0));
+    }
+
+    private static Player createPlayer(Map<String, List<Object>> player) {
+        return new Player((String) player.get("first_name").get(0), (String) player.get("last_name").get(0),
+                (String) player.get("username").get(0), (String) player.get("player_id").get(0),
+                (String) player.get("hash_password").get(0), (String) player.get("email_address").get(0),
+                (String) player.get("phone_number").get(0),(Double) player.get("money").get(0));
+    }
+
+
+    public static Admin getAdmin() {
+        java.util.Map<String, Object> newMap = new HashMap<>();
+        newMap.put("is_admin", 1);
+        Map<String, List<Object>> thisAccount =
+                SQLConnector.selectFromDatabase(newMap, "players");
+        if (thisAccount == null || thisAccount.isEmpty()) {
+            System.err.println("[DATABASE]: Admin entry couldn't be found");
+            return null;
+        }
+        return createAdmin(thisAccount);
+    }
+
+    private static Map<String, List<Object>> SQLAccountSearch(String column,String value) {
+        java.util.Map<String, Object> newMap = new HashMap<>();
+        newMap.put(column, value);
+        Map<String, List<Object>> thisAccount =
+                SQLConnector.selectFromDatabase(newMap, "players");
+        if (thisAccount == null || thisAccount.isEmpty()) {
+            System.err.println("[MODEL]: Account with " + column + " = " + value + " couldn't be found");
+            return null;
+        }
+        return thisAccount;
     }
 
     public static Account getAccountByUsername(String username) {
-        for (Account account : allAccounts) {
-            if (account.username.equals(username)) {
-                return account;
-            }
+        Map<String, List<Object>> thisAccount = SQLAccountSearch("username", username);
+        if (thisAccount == null || thisAccount.isEmpty()) {
+            return null;
         }
-        return null;
+        if (thisAccount.get("is_admin").get(0).equals(1)) {
+            return createAdmin(thisAccount);
+        } else {
+            return createPlayer(thisAccount);
+        }
     }
+
+
 
     public static Account getAccountById(String id) {
-        for (Account account : allAccounts) {
-            if (account.accountId.equals(id)) {
-                return account;
-            }
+        Map<String, List<Object>> thisAccount = SQLAccountSearch("player_id", id);
+        if (thisAccount == null || thisAccount.isEmpty()) {
+            return null;
         }
-        return null;
+        if (thisAccount.get("is_admin").get(0).equals(1)) {
+            return createAdmin(thisAccount);
+        } else {
+            return createPlayer(thisAccount);
+        }
     }
 
-    public static ArrayList<Player> getAllPlayers() {
+    /*public static ArrayList<Player> getAllPlayers() {
         ArrayList<Player> players = new ArrayList<>();
         for (Account account : allAccounts)
             if (account instanceof Player)
@@ -331,7 +341,7 @@ public abstract class Account extends Application {
         while (reader.hasNextLine()) json.append(reader.nextLine());
         reader.close();
         return json;
-    }
+    }*/
 
     public static String saveImageToFile(Image image, String accountId) {
 
@@ -349,11 +359,11 @@ public abstract class Account extends Application {
             throw new RuntimeException(e);
         }
     }
-
+/*
     public static void openFileToImage(Account account) {
-        File file = new File("database\\accounts\\images\\"+account.getAccountId()+".jpg");
+        File file = new File("database\\accounts\\images\\" + account.getAccountId() + ".jpg");
         account.setImage(file.toURI().toString());
-    }
+    }*/
 
     @Override
     public String toString() {
@@ -365,5 +375,4 @@ public abstract class Account extends Application {
                         "\nphoneNumber: " + phoneNumber + "\n"
                 ;
     }
-
 }
