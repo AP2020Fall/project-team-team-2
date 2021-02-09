75d6df33edfb7f65b0ff743d994ee6401c17db08
alxelr2014 <I@$mint16p17>
2021-02-09 14:28:10 +0330

    fix sql

--- src/main/resources/dataSql/messages.sql
+++
@@ -1,9 +0,0 @@
-create table messages
-(
-	id int auto_increment
-		primary key,
-	message_text varchar(50) null,
-	message_id varchar(30) null,
-	timestamp varchar(50) null
-);
-
--- src/main/resources/dataSql/suggestions.sql
+++
@@ -1,9 +0,0 @@
-create table suggestions
-(
-	id int auto_increment
-		primary key,
-	game_name varchar(30) null,
-	suggestion_id varchar(30) null,
-	player_name varchar(20) null
-);
-
